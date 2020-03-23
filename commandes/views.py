c3e174c26b366ca6abafd1af81cc5500d1fbafea
Gilles Bassière <gbassiere@gmail.com>
2020-03-23 15:32:34 +0100

    Add a basic view: merchant can check needed quantities for a delivery

--- commandes/views.py
+++ commandes/views.py
@@ -1,3 +1,27 @@
+import datetime
 from django.shortcuts import render
+from django.db.models import Sum
 
-# Create your views here.
+from .models import Delivery, UnitTypes
+
+
+def needed_quantities(request):
+    """Quantities needed for each delivery"""
+    deliveries = Delivery.objects.filter(slot_date__gte=datetime.date.today()) \
+                                 .values("location__name", "slot_date") \
+                                 .distinct()
+    context = {'deliveries': []}
+    units = dict(UnitTypes.choices)
+    for d in deliveries:
+        context['deliveries'].append({
+            "location": d['location__name'],
+            "date": d['slot_date'],
+            "orders": [{"label": l, "unit": units[u], "quantity": q} for (l, u, q) in
+                       Delivery.objects.filter(location__name=d['location__name'],
+                                              slot_date=d['slot_date']) \
+                                       .values_list("carts__items__label",
+                                               "carts__items__unit_type") \
+                                       .annotate(quantity=Sum("carts__items__quantity"))]})
+
+
+    return render(request, "commandes/index.html", context)
--- potacommandes/urls.py
+++ potacommandes/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import commandes.views
 
 urlpatterns = [
+    path('', commandes.views.needed_quantities),
     path('admin/', admin.site.urls),
 ]
