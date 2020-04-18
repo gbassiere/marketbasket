a3c6d9216d33784712ac72f10d72826b92e08876
Gilles Bassière <gbassiere@gmail.com>
2020-04-18 19:39:48 +0200

    Proper validation of delivery dates in admin

--- commandes/admin.py
+++ commandes/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 from .models import Article, Merchant, Delivery, DeliveryLocation, URL
+from django import forms
+
+
+class DeliveryForm(forms.ModelForm):
+    class Meta:
+        model = Delivery
+        fields = '__all__'
+
+    def clean(self):
+        if self.cleaned_data.get('start') > self.cleaned_data.get('end'):
+            msg = _('Delivery cannot end before it starts')
+            raise forms.ValidationError(msg, code='invalid')
+        return self.cleaned_data
+
+class DeliveryAdmin(admin.ModelAdmin):
+    form = DeliveryForm
+
 
 admin.site.register(Merchant)
 admin.site.register(URL)
 admin.site.register(Article)
-admin.site.register(Delivery)
+admin.site.register(Delivery, DeliveryAdmin)
 admin.site.register(DeliveryLocation)
